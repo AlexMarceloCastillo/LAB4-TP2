1ccea9320a85cce8116d65198d9eedf92c009b62
AlexMarceloCastillo <alexandermarcelocastillo@gmail.com>
2020-03-31 13:16:15 -0300

    AA

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+
+
+ ?>
+<script type="text/javascript">
+  window.addEventListener('load',function(){
+    alert('a');
+  })
+</script>
---
+++ paises.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v11.11 (32 bit)
+MySQL - 5.5.5-10.1.37-MariaDB : Database - tp2la4
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tp2la4` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `tp2la4`;
+
+/*Table structure for table `pais` */
+
+DROP TABLE IF EXISTS `pais`;
+
+CREATE TABLE `pais` (
+  `codigoPais` int(10) NOT NULL,
+  `nombrePais` varchar(50) NOT NULL,
+  `capitalPais` varchar(50) NOT NULL,
+  `region` varchar(50) NOT NULL,
+  `poblacion` bigint(20) NOT NULL,
+  `latitud` decimal(10,0) NOT NULL,
+  `longitud` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`codigoPais`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `pais` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
